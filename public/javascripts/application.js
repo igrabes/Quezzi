f0355f9926fc59e145b6dc3060238687901dc875
Ian <ian.grabill@gmail.com>
2011-05-24 01:11:18 -0400

    Trying to fix jquery5

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
-  var faye = new Faye.Client('http://localhost:9292/faye');
+  var faye = new Faye.Client('http://quiet-flower-596.heroku.com/faye');  // Change Url here
   faye.subscribe("/messages", function(data) {
     eval(data);
   });
 })
+
+
+
+// http://localhost:9292/faye
