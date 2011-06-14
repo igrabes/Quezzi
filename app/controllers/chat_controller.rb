77f2353c924cfa6db6efba8393349b301e1d6c77
Ian <ian.grabill@gmail.com>
2011-06-14 00:31:55 -0400

    fixing this

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,18 +6,18 @@ class ChatController < ApplicationController
   
   
   
-  # def new
-  #     chat = Chat.new
-  #     chat.owner = ChatUser.user(session)
-  #     if chat.save
-  #       chat_tiny = Tiny::tiny(chat.id)
-  #         chat.channel = "message_channel_" + chat.id.to_s
-  #         chat.save
-  #       redirect_to :action => "view", :id => chat_tiny
-  #      else
-  #        redirect_to authentications_url
-  #     end
-  #   end
+  def new
+        chat = Chat.new
+        chat.owner = ChatUser.user(session)
+        if chat.save
+          chat_tiny = Tiny::tiny(chat.id)
+            chat.channel = "message_channel_" + chat.id.to_s
+            chat.save
+          redirect_to :action => "view", :id => chat_tiny
+         else
+           redirect_to authentications_url
+        end
+      end
 
    def view
 
